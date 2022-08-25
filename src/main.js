b43c1aa81f333c37b942e82a5b69ee38e314e14d
Yury Maruk <maruk.dev@gmail.com>
2022-08-25 12:56:59 +0300

    feat: add choice of eldritch

--- src/main.js
+++ src/main.js
@@ -1,4 +1,17 @@
 import difficulties from './data/difficulties';
 import ancientsData from './data/ancients';
 
-console.log(ancientsData[3].id);
+
+const beast1 = document.querySelector('.eldritch1'),
+    beast2 = document.querySelector('.eldritch2'),
+    beast3 = document.querySelector('.eldritch3'),
+    beast4 = document.querySelector('.eldritch4');
+
+let chosenBeast = null;
+
+/* Event listeners */
+
+beast1.addEventListener('click', () => {chosenBeast = ancientsData[0]});
+beast2.addEventListener('click', () => {chosenBeast = ancientsData[1]});
+beast3.addEventListener('click', () => {chosenBeast = ancientsData[2]});
+beast4.addEventListener('click', () => {chosenBeast = ancientsData[3]});
