d507ba0c81059e52c6b0b14802686c1c90660175
Yury Maruk <maruk.dev@gmail.com>
2022-08-26 11:57:36 +0300

    feat: add second stage

--- src/main.js
+++ src/main.js
@@ -21,8 +21,8 @@ beast2.addEventListener('click', () => { chosenBeast = ancientsData[1] });
 beast3.addEventListener('click', () => { chosenBeast = ancientsData[2] });
 beast4.addEventListener('click', () => { chosenBeast = ancientsData[3] });
 
-btmKnead.addEventListener('click', () => {});
-
+/* btmKnead.addEventListener('click', () => {});
+ */
 
 
 /*Функция возвращает случайно число от min по max*/
@@ -33,16 +33,20 @@ btmKnead.addEventListener('click', () => {});
     return Math.abs(Math.floor(rand));
 } */
 
-/* Функция тусует колоду карт*/
+/* Функция тусует  и возвращает колоду карт*/
 function shuffle(array) {
-    array.sort(() => Math.random() - 0.5);
+    for (let i = array.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
 }
 
-/* Функция собирает колоду по цвету для древнего*/
+/* Функция тусует колоду и собирает колоду по цвету для древнего*/
 function createDesk(numberOfCards, deskOfCards) {
     let arr = [];
     shuffle(deskOfCards);
-    for (i = 0; i < numberOfCards; i++) {
+    for (let i = 0; i < numberOfCards; i++) {
         arr.push(deskOfCards[i]);
     }
     return arr;
@@ -51,22 +55,36 @@ function createDesk(numberOfCards, deskOfCards) {
 
 
 /* Шаг №1. Создаем три колоды карт зеленого, коричневого и голубого цвета для определенного древнего и перетусовываем каждую*/
-
+btmKnead.addEventListener('click', () => {
     let numGreenForBeast = chosenBeast.firstStage.greenCards + chosenBeast.secondStage.greenCards + chosenBeast.thirdStage.greenCards;
     let numBlueForBeast = chosenBeast.firstStage.blueCards + chosenBeast.secondStage.blueCards + chosenBeast.thirdStage.blueCards;
     let numBrownForBeast = chosenBeast.firstStage.brownCards + chosenBeast.secondStage.brownCards + chosenBeast.thirdStage.brownCards;
 
-    let deskOfGreen = [];
-    let deskOfBrown = [];
-    let deskOfBlue = [];
+    let deskOfGreen = createDesk(numGreenForBeast, greenCards);
+    let deskOfBrown = createDesk(numBrownForBeast, brownCards);
+    let deskOfBlue = createDesk(numBlueForBeast, blueCards);
 
-    let deskFotFirstStage = [];
-    let deskFotSecondtStage = [];
-    let deskFotThirtytStage = [];
+    let deskFotFirstStage = shuffle(shuffle(
+        [].concat(createDesk(chosenBeast.firstStage.greenCards, deskOfGreen),
+            createDesk(chosenBeast.firstStage.brownCards, deskOfBrown),
+            createDesk(chosenBeast.firstStage.blueCards, deskOfBlue))));
+            console.log(deskFotFirstStage);
+    let deskFotSecondtStage = shuffle(shuffle(
+        [].concat(createDesk(chosenBeast.secondStage.greenCards, deskOfGreen),
+            createDesk(chosenBeast.secondStage.brownCards, deskOfBrown),
+            createDesk(chosenBeast.secondStage.blueCards, deskOfBlue))));
+            console.log(deskFotSecondtStage);
+    let deskFotThirtytStage = shuffle(shuffle(
+        [].concat(createDesk(chosenBeast.thirdStage.greenCards, deskOfGreen),
+            createDesk(chosenBeast.thirdStage.brownCards, deskOfBrown),
+            createDesk(chosenBeast.thirdStage.blueCards, deskOfBlue))));
+            console.log(deskFotThirtytStage);
 
     let finalDesk = [].concat(deskFotFirstStage, deskFotSecondtStage, deskFotThirtytStage);
+});
 
-    console.log(numGreenForBeast, numBlueForBeast, numBrownForBeast);
+
+
 
 
 
